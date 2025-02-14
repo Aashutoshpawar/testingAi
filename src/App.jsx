9c828123ae8c1654e45f2a0f360e5fc53566d405
AashutoshPawar <aashutoshpawar123@gmail.com>
2025-02-15 00:18:15 +0530

    all clear

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const App = () => {
+  const [name, setName] = useState('');
+
+  const handleChange = (event) => {
+    setName(event.target.value); // Update state with input value
+  };
+
   return (
-    <div>App</div>
-  )
+    <>
+      <input type="text" name="text" onChange={handleChange} />
+      <h2 style={{ backgroundColor: "black", color: "white" }}>{name}</h2>
+    </>
+  );
 }
 
-export default App
+export default App;
